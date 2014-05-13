ce52007a63a116823b6e90202fdc858fc17bacba
hraboviyvadim <hraboviyvadim@gmail.com>
2014-05-13 18:02:35 +0300

    upd

--- js/common.js
+++ js/common.js
@@ -1,3 +1,24 @@
 head.ready(function() {
-	console.log($('body').html());
+	
+	// promo
+
+		function promo(){
+			var height = $(window).height();
+			$(".js-promo").css('height', height);
+		}
+		promo();
+
+		
+
+
+
+
+
+
+
+
+		$(window).resize(function(){
+			promo();
+		});
+
 });
